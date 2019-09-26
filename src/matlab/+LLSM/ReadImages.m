2be7f06aec4a20df74a7eb36b0b52ee7dc31c8bf
Eric Wait <eric@waitphoto.com>
2019-09-26 09:43:37 -0400

    Made a generalized file reader for original file name scheme.

---
+++ src/matlab/+LLSM/ReadImages.m
@@ -0,0 +1,32 @@
+function [im,metadata] = ReadImages(imageDir,frames, channels, optionalTxtPath)
+    if (~exist('frames','var'))
+        frames = [];
+    end
+    
+    if (~exist('channels','var'))
+        channels = [];
+    end
+
+    if (~exist('optionalTxtPath','var'))
+        optionalTxtPath = [];
+    end
+
+    tifList = dir(fullfile(imageDir,'*.tif'));
+    klbList = dir(fullfile(imageDir,'*.klb'));
+    
+    ext = 'tif';
+    if (isempty(tifList))
+        if (isempty(klbList))
+            error('Cannot find image files!');
+        end
+        ext = 'klb';
+    elseif (~isempty(klbList))
+        if (length(tifList)<length(klbList))
+            ext = 'klb';
+        end
+    end  
+
+    [metadata, names] = LLSM.GetFileReadingMetadata(imageDir,ext,optionalTxtPath);
+
+    im = LLSM.ReadImageOrgName(metadata,names,frames,channels);
+end
