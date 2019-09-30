d0e6bef6a3827c286b54f108c3beb9e49acd3830
Eric Wait <eric@waitphoto.com>
2019-09-30 10:18:44 -0400

    Make appropriate image containers that do not break when the image reader failed

--- src/matlab/+LLSM/ReadImageOrgName.m
+++ src/matlab/+LLSM/ReadImageOrgName.m
@@ -10,34 +10,38 @@ function im = ReadImageOrgName(metadata, names, frames, channels)
         channels = 1:length(names.uniqueCams) * length(names.uniqueChans);
     end
 
-    im = TimeLoop(metadata,names,frames(1),channels);
+    im = TimeLoop(metadata,names,0,'uint16',frames(1),channels);
     if (length(frames)==1)
         return
     end
     
-    im = zeros([size(im),length(frames)],'like',im);
+    sz = size(im);
+    cl = class(im);
+    im = zeros([sz,length(frames)],'like',im);
     
     [~,~,ext] = fileparts(names.fileNames{1});
     
     if (strcmp(ext,'.tif'))
         parfor t=1:length(frames)
-            curIm = TimeLoop(metadata,names,frames(t),channels);
+            curIm = TimeLoop(metadata,names,sz,cl,frames(t),channels);
             im(:,:,:,:,t) = curIm;
         end
     else
         for t=1:length(frames)
-            curIm = TimeLoop(metadata,names,frames(t),channels);
+            curIm = TimeLoop(metadata,names,sz,cl,frames(t),channels);
             im(:,:,:,:,t) = curIm;
         end
     end
 end
 
-function im = TimeLoop(metadata,names,frame,channels)
+function im = TimeLoop(metadata,names,sz,cl,frame,channels)
     if (names.useStacks)
         timeMask = names.stacks==frame-1;
     else
         timeMask = names.iter==frame-1;
     end
+    
+    im = zeros(sz,cl);
 
     for c=1:length(channels)
         channel = channels(c);
@@ -58,14 +62,12 @@ function im = TimeLoop(metadata,names,frame,channels)
 
         if (~any(fMask))
             warning('Cannot find file for frame: %d channel: %d name: %s',frame,channels(c),metadata.imageDir);
-            im = [];
             return
         end
 
         fName = names.fileNames{fMask};
         if (~exist(fullfile(metadata.imageDir,fName),'file'))
             warning('Cannot find file for frame: %d channel: %d name: %s',frame,channels(c),metadata.imageDir);
-            im = [];
             return
         end
 
@@ -78,11 +80,9 @@ function im = TimeLoop(metadata,names,frame,channels)
         else
             error('Unknown file extention!');
         end
+        
+        szTemp = size(tempIm);
 
-        if (~exist('im','var'))
-            im = zeros([size(tempIm),length(channels)],'like',tempIm);
-        end
-
-        im(:,:,:,c) = tempIm;
+        im(1:szTemp(1),1:szTemp(2),1:szTemp(3),c) = tempIm;
     end
 end
