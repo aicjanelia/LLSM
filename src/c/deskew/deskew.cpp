2e2fa8aaa42deb859ded700a6e0a4992aa997be6
Blair Rossetti <rossettib@janelia.hhmi.org>
2020-09-16 19:52:22 -0400

    added function to load image

--- src/c/deskew/deskew.cpp
+++ src/c/deskew/deskew.cpp
@@ -1,19 +1,27 @@
 #define VERSION "AIC Deskew version 0.1.0"
-#define cimg_display  1
 #define UNSET_FLOAT -1.0f
 #define UNSET_INT -1
 #define UNSET_BOOL false
+#define cimg_use_tiff
 
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
+#include <exception>
 #include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
 #include <CImg.h>
 
-using namespace cimg_library;
+namespace cil = cimg_library;
 namespace po = boost::program_options;
 namespace fs = boost::filesystem;
+
+cil::CImg<> LoadImage(std::string path);
+
+// int SaveImage(cil::CImg img, std::string path);
+
+// template <typename T>
+// cil::CImg Deskew(cil::CImg<T> img, float xy_rez, float z_rez, float angle, int nthreads);
 
 int main(int argc, char *argv[]) {
   // parameters
@@ -23,30 +31,38 @@ int main(int argc, char *argv[]) {
   int nthreads = UNSET_INT;
 
   // declare the supported options
-  po::options_description desc("Allowed options");
-  desc.add_options()
+  po::options_description visible_opts("usage: deskew [options] path\n\nAllowed options");
+  visible_opts.add_options()
       ("help,h", "display this help message")
       ("xy-rez,x", po::value<float>(&xy)->default_value(0.104f), "x/y resolution (um/px)")
       ("z-rez,z", po::value<float>(&z)->default_value(0.25f), "z resolution (um/px)")
       ("angle,a", po::value<float>(&angle)->default_value(32.8f), "deskewing angle (degrees)")
       ("nthreads,n", po::value<int>(&nthreads)->default_value(4),"number of threads")
       ("output,o", po::value<std::string>()->required(),"output file path")
-      ("input,i", po::value<std::string>()->required(), "input file path")
       ("version,v", "display the version number")
   ;
 
-  po::positional_options_description posdesc; 
-  posdesc.add("input,i", 1);
+  po::options_description hidden_opts;
+  hidden_opts.add_options()
+    ("input", po::value<std::string>()->required(), "input file path")
+  ;
+
+  po::positional_options_description positional_opts; 
+  positional_opts.add("input", 1);
+
+  po::options_description all_opts;
+  all_opts.add(visible_opts).add(hidden_opts);
 
   // parse options
   po::variables_map varsmap;
   try {
-    po::store(po::command_line_parser(argc, argv).options(desc).positional(posdesc).run(), varsmap);
+    po::store(po::command_line_parser(argc, argv).options(all_opts).positional(positional_opts).run(), varsmap);
 
     // print help message
     if (varsmap.count("help") || (argc == 1)) {
-        std::cerr << desc << std::endl;
-        return EXIT_FAILURE;
+      std::cerr << "deskew: removes skewing induced by sample scanning\n";
+      std::cerr << visible_opts << std::endl;
+      return EXIT_FAILURE;
     }
 
     // print version number
@@ -57,57 +73,89 @@ int main(int argc, char *argv[]) {
     
     // check options
     po::notify(varsmap);
+
   } catch (po::error &e) {
-    std::cerr << "Error: " << e.what() << "\n\n";
-    std::cerr << desc << std::endl;
+    std::cerr << "deskew: " << e.what() << "\n\n";
+    std::cerr << visible_opts << std::endl;
     return EXIT_FAILURE;
   } catch (...) {
-    std::cerr << "Error: unknown error during command line parsing\n\n";
-    std::cerr << desc << std::endl;
+    std::cerr << "deskew: unknown error during command line parsing\n\n";
+    std::cerr << visible_opts << std::endl;
     return EXIT_FAILURE;
   }
 
-  // check input file
-  fs::path inPath(varsmap["input"].as<std::string>());  // avoid repeated path construction below
+  // load image
+  cil::CImg<> img = LoadImage(varsmap["input"].as<std::string>());
 
-  if (fs::exists(inPath)) {
-    if (fs::is_regular_file(inPath)){
-      std::cout << "Processing " << inPath << std::endl;
-    }
-  } else {
-    std::cerr << "Error: " << inPath << " does not exist" << std::endl;
-  }
+  // deskew
 
-
-//   CImg<unsigned char> img(640,400,1,3);  // Define a 640x400 color image with 8 bits per color component.
-//   img.fill(0);                           // Set pixel values to 0 (color : black)
-//   unsigned char purple[] = { 255,0,255 };        // Define a purple color
-//   img.draw_text(100,100,"Hello World",purple); // Draw a purple "Hello world" at coordinates (100,100).
-//   img.display("My first CImg code");             // Display the image in a display window.
-//   return 0;
 
   return EXIT_SUCCESS;
 }
 
-// int deskew(const CImg<> &inBuf, double deskewFactor, CImg<> &outBuf, int extraShift)
-// {
-//   auto nz = inBuf.depth();
-//   auto ny = inBuf.height();
-//   auto nx = inBuf.width();
-//   auto nxOut = outBuf.width();
-// #pragma omp parallel for
-//   for (auto zout=0; zout < nz; zout++)
-//     for (auto yout=0; yout < ny; yout++)
-//       for (auto xout=0; xout < nxOut; xout++) {
-//         const double xin = (xout - nxOut/2. + extraShift) - deskewFactor*(zout-nz/2.) + nx/2.;
+cil::CImg<> LoadImage(std::string path) {
+  // check input path
+  fs::path p(path);
+  if (fs::exists(p)) {
+    if (fs::is_regular_file(p)){
+      std::cout << "Loading " << p << std::endl;
+    }
+  } else {
+    std::cerr << "LoadImage: " << p << " does not exist" << std::endl;
+  }
 
-//         if (xin >= 0 && floor(xin) <= nx-1) {
-//           const double offset = xin - floor(xin);
-//           const int floor_xin = floor(xin);
-//           outBuf(xout, yout, zout) = (1.0 - offset) * inBuf(floor_xin, yout, zout)
-//             + offset * inBuf(floor_xin+1, yout, zout);
-//         }
-//         else
-//           outBuf(xout, yout, zout) = 100.f;
+  // load image
+  cil::CImg<> img;
+  try {
+    img.load(p.c_str());
+  } catch (cil::CImgException &e) {
+    std::cerr << "CImg Library: " << e.what() << std::endl;
+    throw e;
+  } catch (...) {
+    std::cerr << "LoadImage: unknown error during image loading" << std::endl;
+  }
+
+  return img;
+}
+
+// int SaveImage(cil::CImg img, std::string path) {
+//   // check input path
+//   fs::path p(path);
+//   if (fs::exists(p)) {
+//     if (fs::is_regular_file(p)){
+//       std::cout << "Saving " << p << std::endl;
+//     }
+//   } else {
+//     std::cerr << "SaveImage: " << p << " does not exist" << std::endl;
 //   }
+
+//   return EXIT_SUCCESS;
+// }
+
+// template <typename T>
+// cil::CImg Deskew(cil::CImg<T> img, float xy_rez, float z_rez, float angle, int nthreads) {
+//   // int deskew(const CImg<> &inBuf, double deskewFactor, CImg<> &outBuf, int extraShift)
+// // {
+// //   auto nz = inBuf.depth();
+// //   auto ny = inBuf.height();
+// //   auto nx = inBuf.width();
+// //   auto nxOut = outBuf.width();
+// // #pragma omp parallel for
+// //   for (auto zout=0; zout < nz; zout++)
+// //     for (auto yout=0; yout < ny; yout++)
+// //       for (auto xout=0; xout < nxOut; xout++) {
+// //         const double xin = (xout - nxOut/2. + extraShift) - deskewFactor*(zout-nz/2.) + nx/2.;
+
+// //         if (xin >= 0 && floor(xin) <= nx-1) {
+// //           const double offset = xin - floor(xin);
+// //           const int floor_xin = floor(xin);
+// //           outBuf(xout, yout, zout) = (1.0 - offset) * inBuf(floor_xin, yout, zout)
+// //             + offset * inBuf(floor_xin+1, yout, zout);
+// //         }
+// //         else
+// //           outBuf(xout, yout, zout) = 100.f;
+// //   }
+// // }
+
+//   return img;
 // }
