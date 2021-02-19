bf0459191ed3a4c078b139c76bb24b35c080c924
Blair Rossetti <rossettib@janelia.hhmi.org>
2021-02-19 15:43:48 -0500

    initial commit

---
+++ src/python/settings2json.py
@@ -0,0 +1,16 @@
+#! /misc/local/python-3.8.2/bin/python3
+
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Converts an LLSM Settings.txt file into a parsed JSON')
+    parser.add_argument('--input', '-i', dest='path', help='path to Settings.txt file')
+    args = parser.parse_args()
+
+    return args
+
+def main():
+    return False
+
+if __name__ == '__main__':
+    main()
