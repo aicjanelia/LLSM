4a3cc90e0d7e13740e6570aba3d0ceada179c304
Blair Rossetti <rossettib@janelia.hhmi.org>
2021-02-25 22:46:13 -0500

    almost ready to submit jobs for deskew

--- src/python/llsm-pipeline.py
+++ src/python/llsm-pipeline.py
@@ -5,6 +5,7 @@ This program is the entry point for the LLSM pipeline.
 """
 
 import argparse
+import os
 import re
 import json
 from pathlib import Path
@@ -37,23 +38,23 @@ def load_configs(path):
         exit(f'error: root path \'%s\' does not exist' % root)
 
     if 'decon' in configs:
-        if 'psf' in configs['paths']:
-            if 'dir' in configs['paths']['psf']:
-                partial_path = root / configs['paths']['psf']['dir']
-            else:
-                configs['paths']['psf']['dir'] = None
-                partial_path = root
-                print(f'warning: no psf directory provided... using \'%s\'' % root)
-            
-            if 'laser' in configs['paths']['psf']:
-                for key, val in configs['paths']['psf']['laser'].items():
-                    p = partial_path / val
-                    if not p.is_file():
-                        exit(f'error: laser %s psf file \'%s\' does not exist' % (key, p))
-            else:
-                exit(f'error: no psf files provided')
-        else:
+        if 'psf' not in configs['paths']:
             exit(f'error: decon enabled, but no psf parameters found in config file')
+
+        if 'dir' in configs['paths']['psf']:
+            partial_path = root / configs['paths']['psf']['dir']
+        else:
+            configs['paths']['psf']['dir'] = None
+            partial_path = root
+            print(f'warning: no psf directory provided... using \'%s\'' % root)
+        
+        if 'laser' not in configs['paths']['psf']:
+            exit(f'error: no psf files provided')
+
+        for key, val in configs['paths']['psf']['laser'].items():
+            p = partial_path / val
+            if not p.is_file():
+                exit(f'error: laser %s psf file \'%s\' does not exist' % (key, p))
 
     return configs
 
@@ -86,8 +87,57 @@ def get_dirs(path, excludes):
 
     return unprocessed_dirs
 
+def process(dirs, configs):
+    processed = {}
+    params_bsub = {
+        '-J': 'llsm-pipeline',
+        '-o': '/dev/null',
+        '-We': 10,
+        '-n': 4,
+    }
+    params_deskew = {
+        "-x": 0.104,
+        "-a": 31.8,
+        "-f": 0.0,
+        "-w": False
+    }
+    params_decon = {}
 
-def process
+    # update default params with user defined configs
+    if 'bsub' in configs:
+        params_bsub.update(configs['bsub'])
+    if 'deskew' in configs:
+        params_deskew.update(configs['deskew'])
+
+    # process each directory
+    for d in dirs:
+        print(f'processing \'%s\'...' % d)
+
+        # get list of files
+        files = os.listdir(d)
+
+        # parse the first Settings.txt we find
+        for f in reversed(files):
+            if f.endswith('Settings.txt'):
+                print(f'parsing \'%s\'...' % f)
+                settings = settings2json.parse_txt(d / f)
+                break
+
+        # deskew
+        if 'waveform' not in settings:
+            exit(f'error: settings file did not contain a Waveform section')
+        if 'z-motion' not in settings['waveform']:
+            exit(f'error: settings file did not contain a Z motion field')
+
+        if settings['waveform']['z-motion'] == 'Sample piezo':
+            if 's-piezo' not in settings['waveform']:
+                exit(f'error: settings file did not contain a S Piezo field')
+            if 'interval' not in settings['waveform']['s-piezo']:
+                exit(f'error: settings file did not contain a S Piezo Interval field')
+
+            steps = settings['waveform']['s-piezo']['interval']
+
+    return  processed
 
 if __name__ == '__main__':
     # get command line arguments
@@ -99,20 +149,21 @@ if __name__ == '__main__':
     # get dictionary of processed files
     root_dir = Path(configs['paths']['root'])
     processed_path = root_dir / 'processed.json'
-    processed = get_processed_json(processed_path)
+    processed_json = get_processed_json(processed_path)
 
     # get all directories containing a Settings files
-    excludes = list(processed)
+    excludes = list(processed_json)
     if configs['paths']['psf']['dir'] != None:
         excludes.append(root_dir / configs['paths']['psf']['dir'])
 
     unprocessed_dirs = get_dirs(root_dir, excludes)
 
     # process images in new diectories
-    new_processed = process(unprocessed_dirs, configs)
+    processed_dirs = process(unprocessed_dirs, configs)
 
     # update processed.json
-    processed.update(new_processed)
-    with Path(root_dir)
+    processed_json.update(processed_dirs)
+    with processed_path.open(mode='w') as f:
+        f.write(json.dumps(processed_dirs, indent=4))
 
     print('Done')
