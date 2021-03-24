f58e73f473258577c231edfdfb17db4472034573
Blair Rossetti <rossettib@janelia.hhmi.org>
2021-03-24 17:01:54 -0400

    removed input args for consitency

--- src/c/tests/writer-test.cpp
+++ src/c/tests/writer-test.cpp
@@ -8,8 +8,7 @@
 #include <itkImageFileWriter.h>
 // #include <itkMinimumMaximumImageCalculator.h>
 
-int
-main(int argc, char * argv[])
+int main()
 {
   std::string input_file_name("/nrs/aic/instruments/llsm/pipeline-test/no-deskew/2020-03-11/4-TL10min/642-4ms-20p_488-10ms-20p_25um_CamA_ch1_CAM1_stack0000_488nm_0000000msec_0023277664msecAbs_000x_000y_000z_0000t.tif");
 
