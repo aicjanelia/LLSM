5d390951a366beb349f490eba45a4a78cb84bad9
Blair Rossetti <rossettib@janelia.hhmi.org>
2021-03-26 10:10:32 -0400

    overload to handle no conversion

--- src/c/utils/writer.h
+++ src/c/utils/writer.h
@@ -18,3 +18,13 @@ void WriteImageFile(itk::SmartPointer<TImageIn> image_in, std::string file_path_
   writer->SetInput(image_output);
   writer->Update();
 }
+
+template <class TImage>
+void WriteImageFile(itk::SmartPointer<TImage> image, std::string file_path_out)
+{
+  using WriterType = itk::ImageFileWriter<TImage>;
+  typename WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName(file_path_out);
+  writer->SetInput(image);
+  writer->Update();
+}
