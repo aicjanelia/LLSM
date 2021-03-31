a4589ea6444c0cf7e8c17cb041a7204220a0e507
Blair Rossetti <rossettib@janelia.hhmi.org>
2021-03-31 12:58:13 -0400

    tester for deskew

---
+++ src/c/tests/deskew-test.cpp
@@ -0,0 +1,89 @@
+#include "defines.h"
+#include "utils.h"
+#include "reader.h"
+#include "writer.h"
+
+#include <itkImage.h>
+#include <itkImageBase.h>
+#include <itkImageFileWriter.h>
+#include <itkResampleImageFilter.h>
+#include "itkAffineTransform.h"
+#include "itkLinearInterpolateImageFunction.h"
+
+itk::SmartPointer<kImageType> Deskew(itk::SmartPointer<kImageType> img, double angle, double stage_move_distance, double xy_res, unsigned short fill_value)
+{
+    // compute shift
+    const double shift = stage_move_distance * cos(angle * M_PI/180.0) / xy_res;
+
+    // set up resample filter
+    using FilterType = itk::ResampleImageFilter<kImageType, kImageType>;
+    FilterType::Pointer filter = FilterType::New();
+
+    using TransformType = itk::AffineTransform<double, kDimensions>;
+    TransformType::Pointer transform = TransformType::New();
+    transform->Shear(0, 2, -shift);
+    filter->SetTransform(transform);
+
+    using InterpolatorType = itk::LinearInterpolateImageFunction<kImageType, double>;
+    InterpolatorType::Pointer interpolator = InterpolatorType::New();
+    filter->SetInterpolator(interpolator);
+
+    filter->SetDefaultPixelValue(0.0);
+
+    // calculate output size
+    kImageType::SizeType size = img->GetLargestPossibleRegion().GetSize();
+    std::cout << "Old Size:" << size[0] << ", " << size[1] << ", " << size[2] << std::endl;
+    size[0] = ceil(size[0] + (fabs(shift) * (size[2]-1)));
+    std::cout << "New Size:" << size[0] << ", " << size[1] << ", " << size[2] << std::endl;
+
+    filter->SetSize(size);
+
+    // perform deskew
+    filter->SetInput(img);
+    filter->Update();
+
+    // set spacing
+    kImageType::SpacingType spacing;
+
+    spacing[0] = xy_res;
+    spacing[1] = xy_res;
+    spacing[2] = stage_move_distance * sin(angle * M_PI/180.0);
+
+    img->SetSpacing(spacing);
+
+    std::cout << spacing[0] << ", " << spacing[1] << ", " << spacing[2] << std::endl;
+
+    return filter->GetOutput();
+}
+
+int main()
+{
+    // parameters
+    std::string in_path("skewed-image-c1.tif");
+
+    double xy_res = 0.104;
+    double stage_move_distance = 0.26667;
+    double angle = 31.8;
+    unsigned short fill_value = 0;
+    unsigned int bit_depth = 16;
+    
+    // read image
+    itk::SmartPointer<kImageType> img = ReadImageFile(in_path);
+
+    if (img == nullptr)
+    {
+        std::cerr << "Unable to read " << in_path.c_str() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // deskew
+    itk::SmartPointer<kImageType> deskew_img = Deskew(img, angle, stage_move_distance, xy_res, fill_value);
+
+    // write image
+    using PixelTypeOut = unsigned short;
+    using ImageTypeOut = itk::Image<PixelTypeOut, kDimensions>;
+
+    WriteImageFile<kImageType, ImageTypeOut>(deskew_img,"deskew-test.tif");
+
+    return EXIT_SUCCESS;
+}
