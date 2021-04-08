f4816f4c51bb84da0aabdbb391a5542900406cc3
Eric Wait <eric@waitphoto.com>
2021-04-07 21:46:25 -0400

    Fixed deskew tester

--- src/c/tests/decon-test.cpp
+++ src/c/tests/decon-test.cpp
@@ -8,11 +8,11 @@
 int main()
 {
     // read image
-    std::string image_path("examples/cell2_ch0_CAM1_stack0001_560nm_0004529msec_0009990533msecAbs_000x_000y_000z_0000t.tif");
+    std::string image_path("examples/cell2_ch1_CAM1_stack0001_488nm_0004529msec_0009990533msecAbs_000x_000y_000z_0000t.tif");
     itk::SmartPointer<kImageType> image = ReadImageFile(image_path, true);
 
     // read PSF
-    std::string psf_path("examples/560_PSF_piezoScan.tif");
+    std::string psf_path("examples/488_PSF_piezoScan.tif");
     itk::SmartPointer<kImageType> psf = ReadImageFile(psf_path, true);
 
     if (image == nullptr)
@@ -35,32 +35,25 @@ int main()
     double stage_move_distance = 0.4;
     double angle = 31.8;
     float fill_value = 100.0 / std::numeric_limits<unsigned short>::max();
-    unsigned int bit_depth = 16;
 
     // deskew
     itk::SmartPointer<kImageType> deskew_img = Deskew(image, angle, stage_move_distance, xy_res, fill_value, true);
     image = NULL; // this will clean up the memory
-
-    kImageType::SpacingType deskew_spacing;
-    deskew_spacing[0] = 0.104;
-    deskew_spacing[1] = 0.104;
-    deskew_spacing[2] = stage_move_distance * sin(angle * M_PI/180.0);
-
-    deskew_img->SetSpacing(deskew_spacing);
-
     WriteImageFile<kImageType,ImageTypeOut>(deskew_img,"deskew-in-decon-test.tif", true);
 
     // parameters
     double psf_z_res = 0.4;
 
-    kImageType::SpacingType psf_spacing = deskew_spacing;
+    kImageType::SpacingType psf_spacing = deskew_img->GetSpacing();
     psf_spacing[2] = psf_z_res;
-    itk::SmartPointer<kImageType> psf_resampled = Resampler(psf, psf_spacing, true);
+    psf->SetSpacing(psf_spacing);
+
+    itk::SmartPointer<kImageType> psf_resampled = Resampler(psf, deskew_img->GetSpacing(), true);
     
-    itk::SmartPointer<kImageType> decon_img = RichardsonLucy(deskew_img, psf_resampled, 2, true);
+    itk::SmartPointer<kImageType> decon_img = RichardsonLucy(deskew_img, psf_resampled, 100, true);
 
     // write image
-    decon_img->SetSpacing(deskew_spacing);
+    decon_img->SetSpacing(deskew_img->GetSpacing());
     WriteImageFile<kImageType, ImageTypeOut>(decon_img,"decon-test.tif", true);
 
     return EXIT_SUCCESS;
