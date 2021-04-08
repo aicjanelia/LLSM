5494d14871417a3f69bf7bc306b9bbce9832384e
Eric Wait <eric@waitphoto.com>
2021-04-08 13:47:51 -0400

    Updated decon test

--- src/c/tests/decon-test.cpp
+++ src/c/tests/decon-test.cpp
@@ -4,12 +4,23 @@
 #include "deskew.h"
 #include "resampler.h"
 #include "decon.h"
+#include "utils.h"
+#include "math_local.h"
+
+#include <chrono>
+#include <itkSubtractImageFilter.h>
+#include <itkClampImageFilter.h>
 
 int main()
 {
+    using PixelTypeOut = unsigned short;
+    using ImageTypeOut = itk::Image<PixelTypeOut, kDimensions>;
+    auto start = std::chrono::high_resolution_clock::now();
     // read image
     std::string image_path("examples/cell2_ch1_CAM1_stack0001_488nm_0004529msec_0009990533msecAbs_000x_000y_000z_0000t.tif");
     itk::SmartPointer<kImageType> image = ReadImageFile<kImageType>(image_path, true);
+
+    // itk::SmartPointer<kImageType> image_sub = ConvertImage<ImageTypeOut,kImageType>(clampFilter->GetOutput());
 
     // read PSF
     std::string psf_path("examples/488_PSF_piezoScan.tif");
@@ -27,34 +38,45 @@ int main()
         return EXIT_FAILURE;
     }
 
-    using PixelTypeOut = unsigned short;
-    using ImageTypeOut = itk::Image<PixelTypeOut, kDimensions>;
-
     // parameters
     double xy_res = 0.104;
     double stage_move_distance = 0.4;
     double angle = 31.8;
-    float fill_value = 100.0 / std::numeric_limits<unsigned short>::max();
+    float fill_value = 0.0;//100.0 / std::numeric_limits<unsigned short>::max();
 
     // deskew
     itk::SmartPointer<kImageType> deskew_img = Deskew(image, angle, stage_move_distance, xy_res, fill_value, true);
     image = NULL; // this will clean up the memory
     WriteImageFile<kImageType,ImageTypeOut>(deskew_img,"deskew-in-decon-test.tif", true);
 
+    auto sub_image = SubtractConstantClamped(deskew_img, 100.0 / std::numeric_limits<unsigned short>::max());
+
     // parameters
     double psf_z_res = 0.4;
 
     kImageType::SpacingType psf_spacing = deskew_img->GetSpacing();
+    printf("Deskew spacing: %0.3f, %0.3f, %0.3f\n", psf_spacing[0], psf_spacing[1], psf_spacing[2]);
+
     psf_spacing[2] = psf_z_res;
     psf->SetSpacing(psf_spacing);
 
     itk::SmartPointer<kImageType> psf_resampled = Resampler(psf, deskew_img->GetSpacing(), true);
+
+    WriteImageFile<kImageType,ImageTypeOut>(psf_resampled,"deskew-psf-resampled.tif", true);
     
-    itk::SmartPointer<kImageType> decon_img = RichardsonLucy(deskew_img, psf_resampled, 100, true);
+    itk::SmartPointer<kImageType> decon_img = RichardsonLucy(sub_image, psf_resampled, 5, true);
 
     // write image
     decon_img->SetSpacing(deskew_img->GetSpacing());
     WriteImageFile<kImageType, ImageTypeOut>(decon_img,"decon-test.tif", true);
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = stop - start;
+
+    printf("Decon test took: %2ld:%2ld:%2ld (HH:MM:SS)\n",
+        std::chrono::duration_cast<std::chrono::hours>(duration).count(),
+        std::chrono::duration_cast<std::chrono::minutes>(duration).count(),
+        std::chrono::duration_cast<std::chrono::seconds>(duration).count());
 
     return EXIT_SUCCESS;
 }
