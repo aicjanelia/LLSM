3f05de76530a0adff1dcd64de1b9061f3e5cd74d
Eric Wait <eric@waitphoto.com>
2021-05-20 15:09:43 -0400

    Fixed get range to deal with types correctly

--- src/c/utils/utils.h
+++ src/c/utils/utils.h
@@ -14,59 +14,54 @@
 
 namespace fs = boost::filesystem;
 
-template <class TImage>
-void GetRange(itk::SmartPointer<TImage> image, double &min_val, double &max_val)
+template <class T>
+void GetRange(double &min_val, double &max_val)
 {
-  min_val = 0.0;
-  max_val = 1.0;
-}
-
-void GetRange(itk::SmartPointer<itk::Image<unsigned char, kDimensions>> image, double &min_val, double &max_val)
-{
-  min_val = double(std::numeric_limits<unsigned char>::min());
-  max_val = double(std::numeric_limits<unsigned char>::max());
-}
-
-void GetRange(itk::SmartPointer<itk::Image<char, kDimensions>> image, double &min_val, double &max_val)
-{
-  min_val = double(std::numeric_limits<char>::min());
-  max_val = double(std::numeric_limits<char>::max());
-}
-
-void GetRange(itk::SmartPointer<itk::Image<unsigned short, kDimensions>> image, double &min_val, double &max_val)
-{
-  min_val = double(std::numeric_limits<unsigned short>::min());
-  max_val = double(std::numeric_limits<unsigned short>::max());
-}
-
-void GetRange(itk::SmartPointer<itk::Image<short, kDimensions>> image, double &min_val, double &max_val)
-{
-  min_val = double(std::numeric_limits<short>::min());
-  max_val = double(std::numeric_limits<short>::max());
-}
-
-void GetRange(itk::SmartPointer<itk::Image<unsigned int, kDimensions>> image, double &min_val, double &max_val)
-{
-  min_val = double(std::numeric_limits<unsigned int>::min());
-  max_val = double(std::numeric_limits<unsigned int>::max());
-}
-
-void GetRange(itk::SmartPointer<itk::Image<int, kDimensions>> image, double &min_val, double &max_val)
-{
-  min_val = double(std::numeric_limits<int>::min());
-  max_val = double(std::numeric_limits<int>::max());
-}
-
-void GetRange(itk::SmartPointer<itk::Image<unsigned long int, kDimensions>> image, double &min_val, double &max_val)
-{
-  min_val = double(std::numeric_limits<unsigned long int>::min());
-  max_val = double(std::numeric_limits<unsigned long int>::max());
-}
-
-void GetRange(itk::SmartPointer<itk::Image<long int, kDimensions>> image, double &min_val, double &max_val)
-{
-  min_val = double(std::numeric_limits<long int>::min());
-  max_val = double(std::numeric_limits<long int>::max());
+  if (std::is_same<T, unsigned char>::value)
+  {
+    min_val = double(std::numeric_limits<unsigned char>::min());
+    max_val = double(std::numeric_limits<unsigned char>::max());
+  }
+  else if (std::is_same<T, char>::value)
+  {
+    min_val = double(std::numeric_limits<char>::min());
+    max_val = double(std::numeric_limits<char>::max());
+  }
+  else if (std::is_same<T, unsigned short>::value)
+  {
+    min_val = double(std::numeric_limits<unsigned short>::min());
+    max_val = double(std::numeric_limits<unsigned short>::max());
+  }
+  else if (std::is_same<T, short>::value)
+  {
+    min_val = double(std::numeric_limits<short>::min());
+    max_val = double(std::numeric_limits<short>::max());
+  }
+  else if (std::is_same<T, unsigned int>::value)
+  {
+    min_val = double(std::numeric_limits<unsigned int>::min());
+    max_val = double(std::numeric_limits<unsigned int>::max());
+  }
+  else if (std::is_same<T, int>::value)
+  {
+    min_val = double(std::numeric_limits<int>::min());
+    max_val = double(std::numeric_limits<int>::max());
+  }
+  else if (std::is_same<T, unsigned long int>::value)
+  {
+    min_val = double(std::numeric_limits<unsigned long int>::min());
+    max_val = double(std::numeric_limits<unsigned long int>::max());
+  }
+  else if (std::is_same<T, long int>::value)
+  {
+    min_val = double(std::numeric_limits<long int>::min());
+    max_val = double(std::numeric_limits<long int>::max());
+  }
+  else
+  {
+    min_val = 0.0;
+    max_val = 1.0;
+  }
 }
 
 template <class TImageIn, class TImageOut>
@@ -77,9 +72,9 @@ itk::SmartPointer<TImageOut> ConvertImage(itk::SmartPointer<TImageIn> image_in)
 
   double input_min, input_max, output_min, output_max;
 
-  GetRange(image_in, input_min, input_max);
+  GetRange<TImageIn::PixelType>(input_min, input_max);
   typename itk::SmartPointer<TImageOut> image_out = TImageOut::New();
-  GetRange(image_out, output_min, output_max);
+  GetRange<TImageOut::PixelType>(output_min, output_max);
 
   using RescaleType = itk::IntensityWindowingImageFilter<TImageIn, TImageOut>;
   typename RescaleType::Pointer convert_fltr = RescaleType::New();
