1c6ee29f84727c0932a6b468fc0d2332eb74fdba
Eric Wait <eric@waitphoto.com>
2021-06-02 16:48:44 -0400

    Writer now sets XY spacing relative to the Z spacing

--- src/c/utils/writer.h
+++ src/c/utils/writer.h
@@ -12,6 +12,12 @@ void WriteImageFile(typename TImageIn::Pointer image_in, std::string out_path, b
 {
   typename TImageOut::Pointer image_output = ConvertImage<TImageIn,TImageOut>(image_in);
 
+  // This is a hack because the ITK Tiff writer only writes out the XY resolution in inches
+  TImageIn::SpacingType spacing = image_in->GetSpacing();
+  spacing[0] = (spacing[0] / spacing[2]) * 25.4;
+  spacing[1] = (spacing[1] / spacing[2]) * 25.4;
+  image_output->SetSpacing(spacing);
+
   using WriterType = itk::ImageFileWriter<TImageOut>;
   typename WriterType::Pointer writer = WriterType::New();
   writer->SetFileName(out_path);
