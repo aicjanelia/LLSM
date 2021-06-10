f3e1b1caef846fb9b4c6989b68de85256d695992
Blair Rossetti <rossettib@janelia.hhmi.org>
2021-06-10 10:03:34 -0400

    successfully parses filenames... even ugly ones

--- src/python/filename2json.py
+++ src/python/filename2json.py
@@ -9,6 +9,8 @@ import re
 import json
 from sys import exit
 
+import utils
+
 """
 Parser for command line arguments
 """
@@ -21,18 +23,6 @@ def parse_args():
         print('warning: \'%s\' does not appear to be a LLSM image file\n' % args.input)
 
     return args
-
-"""
-Adds a parsed string to a given dictionary
-"""
-def search_pattern(data, key, string, pattern, cast_as=str):
-    m = re.search(pattern, string)
-
-    if m:
-        if cast_as == str:
-            data[key] = m.group(1).strip()
-        else:
-            data[key] = cast_as(m.group(1).strip())
         
 """
 Converter for v4.04505.Development LLSM TIFF files
@@ -41,31 +31,34 @@ def convert(path):
     data = {}
 
     # parse user-specified name
-    search_pattern(data, 'name', path, r'^Sample :\\t(.*)')
+    utils.find_first_pattern(data, 'name', path, r'^(.*?)_(?:Iter|Cam|ch)')
 
     # parse iter indices
-    search_pattern(data, 'name', path, r'^Sample :\\t(.*)', cast_as=int)
+    utils.find_every_pattern(data, 'iters', path, r'_Iter_(\d+)', cast_as=int)
 
     # parse camera
-    search_pattern(data, 'camera', path, r'^Sample :\\t(.*)')
+    utils.find_last_pattern(data, 'camera', path, r'_Cam([A-B])')
 
     # parse channel
-    search_pattern(data, 'channel', path, r'^Sample :\\t(.*)', cast_as=int)
+    utils.find_last_pattern(data, 'channel', path, r'_ch(\d+)', cast_as=int)
 
     # parse stack
-    search_pattern(data, 'stack', path, r'^X :\t(\d*)', cast_as=int)
+    utils.find_last_pattern(data, 'stack', path, r'_stack(\d+)', cast_as=int)
 
     # parse laser
-    search_pattern(data, 'laser', path, r'^X :\t(\d*)', cast_as=int)
+    utils.find_last_pattern(data, 'laser', path, r'_(\d+)nm', cast_as=int)
 
     # parse relative time
-    search_pattern(data, 'time_rel', path, r'^X :\t(\d*)', cast_as=int)
+    utils.find_last_pattern(data, 'time_rel', path, r'_(\d+)msec_', cast_as=int)
 
     # parse absolute time
-    search_pattern(data, 'time_abs', path, r'^X :\t(\d*)', cast_as=int)
+    utils.find_last_pattern(data, 'time_abs', path, r'_(\d+)msecAbs', cast_as=int)
 
     # parse x,y,z,t indices
-
+    utils.find_last_pattern(data, 'x', path, r'_(\d+)x', cast_as=int)
+    utils.find_last_pattern(data, 'y', path, r'_(\d+)y', cast_as=int)
+    utils.find_last_pattern(data, 'z', path, r'_(\d+)z', cast_as=int)
+    utils.find_last_pattern(data, 't', path, r'_(\d+)t', cast_as=int)
 
     return data
 
