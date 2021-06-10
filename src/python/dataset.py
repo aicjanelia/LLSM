4fee495a651415173178b080dfd7692769865234
Blair Rossetti <rossettib@janelia.hhmi.org>
2021-06-10 12:46:32 -0400

    moved parse checks to Datum object

--- src/python/dataset.py
+++ src/python/dataset.py
@@ -15,21 +15,74 @@ class Datum:
         self.channel = None
         self.stack = None
         self.laser = None
-        self.rel_time = None
-        self.abs_time = None
+        self.time_rel = None
+        self.time_abs = None
         self.x = None
         self.y = None
         self.z = None
         self.t = None
+
+        self.is_macro = None
+        self.is_dualcam = None
 
         self.generate()
 
     def generate(self):
         # parse filename
         j = filename2json(self.path)
+
+        # store json as object variables
+        if 'name' not in j:
+            exit('error: could not parse user-specified name field')
+        self.name = j['name']
+
+        if 'iters' in j:
+            self.is_macro = True
+            self.iters = j['iters']
+        else:
+            self.is_macro = False
+
+        if 'camera' in j:
+            self.is_dualcam = True
+            self.camera = j['camera']
+        else:
+            self.is_dualcam = False
+
+        if 'channel' not in j:
+            exit('error: could not parse channel field')
         self.channel = j['channel']
-        self.camera = j['camera']
-        
+
+        if 'stack' not in j:
+            exit('error: could not parse stack field')
+        self.stack = j['stack']
+
+        if 'laser' not in j:
+            exit('error: could not parse laser field')
+        self.laser = j['laser']
+
+        if 'time_rel' not in j:
+            exit('error: could not parse relative time field')
+        self.time_rel = j['time_rel']
+
+        if 'time_abs' not in j:
+            exit('error: could not parse absolute time field')
+        self.time_abs = j['time_abs']
+
+        if 'x' not in j:
+            exit('error: could not parse x index field')
+        self.x = j['x']
+
+        if 'y' not in j:
+            exit('error: could not parse y index field')
+        self.y = j['y']
+
+        if 'z' not in j:
+            exit('error: could not parse z index field')
+        self.z = j['z']
+
+        if 't' not in j:
+            exit('error: could not parse t index field')
+        self.t = j['t']
 
 class Dataset:
     def __init__(self, path):
--- src/python/filename2json.py
+++ src/python/filename2json.py
@@ -8,7 +8,6 @@ import argparse
 import re
 import json
 from pathlib import Path
-from sys import exit
 
 import utils
 
@@ -36,61 +35,33 @@ def convert(path):
 
     # parse user-specified name
     utils.find_first_pattern(data, 'name', filename, r'^(.*?)_(?:Iter|Cam|ch)')
-    if 'name' not in data:
-        exit('error: could not parse user-specified name field')
 
     # parse iter indices
     utils.find_every_pattern(data, 'iters', filename, r'_Iter_(\d+)', cast_as=int)
-    if 'iters' in data:
-        data['is_macro'] = True
-    else:
-        data['is_macro'] = False
 
     # parse camera
     utils.find_last_pattern(data, 'camera', filename, r'_Cam([A-B])')
-    if 'camera' in data:
-        data['is_dualcam'] = True
-    else:
-        data['is_dualcam'] = False
 
     # parse channel
     utils.find_last_pattern(data, 'channel', filename, r'_ch(\d+)', cast_as=int)
-    if 'channel' not in data:
-        exit('error: could not parse channel field')
 
     # parse stack
     utils.find_last_pattern(data, 'stack', filename, r'_stack(\d+)', cast_as=int)
-    if 'stack' not in data:
-        exit('error: could not parse stack field')
 
     # parse laser
     utils.find_last_pattern(data, 'laser', filename, r'_(\d+)nm', cast_as=int)
-    if 'laser' not in data:
-        exit('error: could not parse laser field')
 
     # parse relative time
     utils.find_last_pattern(data, 'time_rel', filename, r'_(\d+)msec_', cast_as=int)
-    if 'time_rel' not in data:
-        exit('error: could not parse relative time field')
 
     # parse absolute time
     utils.find_last_pattern(data, 'time_abs', filename, r'_(\d+)msecAbs', cast_as=int)
-    if 'time_abs' not in data:
-        exit('error: could not parse absolute time field')
 
     # parse x,y,z,t indices
     utils.find_last_pattern(data, 'x', filename, r'_(\d+)x', cast_as=int)
     utils.find_last_pattern(data, 'y', filename, r'_(\d+)y', cast_as=int)
     utils.find_last_pattern(data, 'z', filename, r'_(\d+)z', cast_as=int)
     utils.find_last_pattern(data, 't', filename, r'_(\d+)t', cast_as=int)
-    if 'x' not in data:
-        exit('error: could not parse x index field')
-    if 'y' not in data:
-        exit('error: could not parse y index field')
-    if 'z' not in data:
-        exit('error: could not parse z index field')
-    if 't' not in data:
-        exit('error: could not parse t index field')
 
     return data
 
