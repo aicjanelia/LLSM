03f55cbd73fc29609b92951af5aba4c8926f4e5f
Eric Wait <eric@waitphoto.com>
2021-06-18 15:13:49 -0400

    Added typename in writer for linux compiler

--- src/c/utils/writer.h
+++ src/c/utils/writer.h
@@ -13,7 +13,7 @@ void WriteImageFile(typename TImageIn::Pointer image_in, std::string out_path, b
   typename TImageOut::Pointer image_output = ConvertImage<TImageIn,TImageOut>(image_in);
 
   // This is a hack because the ITK Tiff writer only writes out the XY resolution in inches
-  TImageIn::SpacingType spacing = image_in->GetSpacing();
+  typename TImageIn::SpacingType spacing = image_in->GetSpacing();
   spacing[0] = (spacing[0] / spacing[2]) * 25.4;
   spacing[1] = (spacing[1] / spacing[2]) * 25.4;
   image_output->SetSpacing(spacing);
