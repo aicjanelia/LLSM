d2f7d234011d553b45b56a727dd2043bfb60f35f
Blair Rossetti <rossettib@janelia.hhmi.org>
2021-06-18 13:50:18 -0400

    fixed strings

--- src/python/llsm-pipeline.py
+++ src/python/llsm-pipeline.py
@@ -14,17 +14,17 @@ from sys import exit
 import settings2json
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Batch deskewing and deconvolution script for LLSM images.')
+    parser = argparse.ArgumentParser(description='Batch deskewing, deconvolution, andmip script for LLSM images.')
     parser.add_argument('input', type=Path, help='path to configuration JSON file')
     parser.add_argument('--dry-run', '-d', default=False, action='store_true', dest='dryrun',help='execute without submitting any bsub jobs')
     parser.add_argument('--verbose', '-v', default=False, action='store_true', dest='verbose',help='print details (including commands to bsub)')
     args = parser.parse_args()
 
     if not args.input.is_file():
-        exit(f'error: \'%s\' does not exist' % args.input)
+        exit('error: \'%s\' does not exist' % args.input)
 
     if not args.input.suffix == '.json':
-        print(f'warning: \'%s\' does not appear to be a settings file\n' % args.input)
+        print('warning: \'%s\' does not appear to be a settings file\n' % args.input)
 
     return args
 
@@ -33,34 +33,34 @@ def load_configs(path):
         try:
             configs = json.load(f)
         except json.JSONDecodeError as e:
-            exit(f'error: \'%s\' is not formatted as a proper JSON file...\n%s' % (path, e))
+            exit('error: \'%s\' is not formatted as a proper JSON file...\n%s' % (path, e))
 
     # sanitize root path
     root = Path(configs["paths"]["root"])
     if not root.is_dir():
-        exit(f'error: root path \'%s\' does not exist' % root)
+        exit('error: root path \'%s\' does not exist' % root)
 
     # sanitize bsub configs
     if 'bsub' in configs:
         supported_opts = ['o', 'We', 'n']
         for key in list(configs['bsub']):
             if key not in supported_opts:
-                print(f'warning: bsub option \'%s\' in config.json is not supported' % key)
+                print('warning: bsub option \'%s\' in config.json is not supported' % key)
                 del configs['bsub'][key]
 
         if 'o' in configs['bsub']:
             if not Path(configs['bsub']['o']).is_dir:
-                exit(f'error: bsub output directory \'%s\' in config.json is not a valid path' % configs['bsub']['o'])
+                exit('error: bsub output directory \'%s\' in config.json is not a valid path' % configs['bsub']['o'])
             configs['bsub']['o'] = {'flag': '-o', 'arg': configs['bsub']['o']}
         
         if 'We' in configs['bsub']:
             if not type(configs['bsub']['We']) is int:
-                exit(f'error: bsub estimated wait time \'%s\' in config.json is not an integer' % configs['bsub']['We'])
+                exit('error: bsub estimated wait time \'%s\' in config.json is not an integer' % configs['bsub']['We'])
             configs['bsub']['We'] = {'flag': '-We', 'arg': configs['bsub']['We']}
         
         if 'n' in configs['bsub']:
             if not type(configs['bsub']['n']) is int:
-                exit(f'error: bsub slot count \'%s\' in config.json is not an integer' % configs['bsub']['n'])
+                exit('error: bsub slot count \'%s\' in config.json is not an integer' % configs['bsub']['n'])
             configs['bsub']['n'] = {'flag': '-n', 'arg': configs['bsub']['n']}
 
     # sanitize deskew configs
@@ -68,22 +68,22 @@ def load_configs(path):
         supported_opts = ['xy-res', 'fill', 'bit-depth']
         for key in list(configs['deskew']):
             if key not in supported_opts:
-                print(f'warning: deskew option \'%s\' in config.json is not supported' % key)
+                print('warning: deskew option \'%s\' in config.json is not supported' % key)
                 del configs['deskew'][key]
 
         if 'xy-res' in configs['deskew']:
             if not type(configs['deskew']['xy-res']) is float:
-                exit(f'error: deskew xy resolution \'%s\' in config.json is not a float' % configs['deskew']['xy-res'])
+                exit('error: deskew xy resolution \'%s\' in config.json is not a float' % configs['deskew']['xy-res'])
             configs['deskew']['xy-res'] = {'flag': '-x', 'arg': configs['deskew']['xy-res']}
         
         if 'fill' in configs['deskew']:
             if not type(configs['deskew']['fill']) is float:
-                exit(f'error: deskew background fill value \'%s\' in config.json is not a float' % configs['deskew']['fill'])
+                exit('error: deskew background fill value \'%s\' in config.json is not a float' % configs['deskew']['fill'])
             configs['deskew']['fill'] = {'flag': '-f', 'arg': configs['deskew']['fill']}
 
         if 'bit-depth' in configs['deskew']:
             if configs['deskew']['bit-depth'] not in [8, 16, 32]:
-                exit(f'error: deskew bit-depth \'%s\' in config.json must be 8, 16, or 32' % configs['deskew']['bit-depth'])
+                exit('error: deskew bit-depth \'%s\' in config.json must be 8, 16, or 32' % configs['deskew']['bit-depth'])
             configs['deskew']['bit-depth'] = {'flag': '-b', 'arg': configs['deskew']['bit-depth']}
 
     # sanitize decon configs
@@ -91,22 +91,22 @@ def load_configs(path):
         supported_opts = ['n', 'bit-depth', 'subtract']
         for key in list(configs['decon']):
             if key not in supported_opts:
-                print(f'warning: decon option \'%s\' in config.json is not supported' % key)
+                print('warning: decon option \'%s\' in config.json is not supported' % key)
                 del configs['decon'][key]
 
         if 'n' in configs['decon']:
             if not type(configs['decon']['n']) is int:
-                exit(f'error: decon iteration number (n) \'%s\' in config.json must be 8, 16, or 32' % configs['decon']['n'])
+                exit('error: decon iteration number (n) \'%s\' in config.json must be 8, 16, or 32' % configs['decon']['n'])
             configs['decon']['n'] = {'flag': '-n', 'arg': configs['decon']['n']}
         
         if 'bit-depth' in configs['decon']:
             if configs['decon']['bit-depth'] not in [8, 16, 32]:
-                exit(f'error: decon bit-depth \'%s\' in config.json must be 8, 16, or 32' % configs['decon']['bit-depth'])
+                exit('error: decon bit-depth \'%s\' in config.json must be 8, 16, or 32' % configs['decon']['bit-depth'])
             configs['decon']['bit-depth'] = {'flag': '-b', 'arg': configs['decon']['bit-depth']}
 
         if 'subtract' in configs['decon']:
             if not type(configs['decon']['subtract']) is float:
-                exit(f'error: decon subtract value \'%s\' in config.json must be a float' % configs['decon']['subtract'])
+                exit('error: decon subtract value \'%s\' in config.json must be a float' % configs['decon']['subtract'])
             configs['decon']['subtract'] = {'flag': '-s', 'arg': configs['decon']['subtract']}
 
         if 'psf' not in configs['paths']:
@@ -117,15 +117,15 @@ def load_configs(path):
         else:
             configs['paths']['psf']['dir'] = None
             partial_path = root
-            print(f'warning: no psf directory provided... using \'%s\'' % root)
+            print('warning: no psf directory provided... using \'%s\'' % root)
         
         if 'laser' not in configs['paths']['psf']:
-            exit(f'error: no psf files provided')
+            exit('error: no psf files provided')
 
         for key, val in configs['paths']['psf']['laser'].items():
             p = partial_path / val
             if not p.is_file():
-                exit(f'error: laser %s psf file \'%s\' does not exist' % (key, p))
+                exit('error: laser %s psf file \'%s\' does not exist' % (key, p))
 
     return configs
 
@@ -135,7 +135,7 @@ def get_processed_json(path):
             try:
                 processed = json.load(f)
             except json.JSONDecodeError as e:
-                exit(f'error: \'%s\' is not formatted as a proper JSON file...\n%s' % (path, e))
+                exit('error: \'%s\' is not formatted as a proper JSON file...\n%s' % (path, e))
         
         return processed
     else:
@@ -166,7 +166,7 @@ def tag_filename(filename, string):
 def params2cmd(params, cmd_name):
     cmd = cmd_name
     for key in params:
-        cmd += f' %s %s' % (params[key]['flag'], params[key]['arg'])
+        cmd += ' %s %s' % (params[key]['flag'], params[key]['arg'])
 
     return cmd
 
@@ -216,7 +216,7 @@ def process(dirs, configs, dryrun=False, verbose=False):
             filename, _ = os.path.splitext(val)
             p = root / configs['paths']['psf']['dir'] / (filename + '_Settings.txt')
             if not p.is_file():
-                exit(f'error: laser %s psf file \'%s\' does not have a Settings file' % (key, p))
+                exit('error: laser %s psf file \'%s\' does not have a Settings file' % (key, p))
    
             settings = settings2json.parse_txt(p)
 
@@ -226,11 +226,11 @@ def process(dirs, configs, dryrun=False, verbose=False):
             elif settings['waveform']['z-motion'] == 'Z galvo & piezo':
                 psf_settings[key]['z-step'] = settings['waveform']['z-pzt']['interval'][0] 
             else:
-                exit(f'error: PSF z-motion cannot be determined for laser %s' % key)
+                exit('error: PSF z-motion cannot be determined for laser %s' % key)
 
     # process each directory
     for d in dirs:
-        print(f'processing \'%s\'...' % d)
+        print('processing \'%s\'...' % d)
 
         # get list of files
         files = os.listdir(d)
@@ -238,16 +238,16 @@ def process(dirs, configs, dryrun=False, verbose=False):
         # parse the last Settings.txt file
         for f in reversed(files):
             if f.endswith('Settings.txt'):
-                print(f'parsing \'%s\'...' % f)
+                print('parsing \'%s\'...' % f)
                 settings = settings2json.parse_txt(d / f)
                 pattern = re.compile(f.split('_')[0] + '.*_ch(\d+).*\.tif')
                 break
 
         # check for z-motion settings
         if 'waveform' not in settings:
-            exit(f'error: settings file did not contain a Waveform section')
+            exit('error: settings file did not contain a Waveform section')
         if 'z-motion' not in settings['waveform']:
-            exit(f'error: settings file did not contain a Z motion field')
+            exit('error: settings file did not contain a Z motion field')
 
         # save settings json
         if not dryrun:
@@ -258,9 +258,9 @@ def process(dirs, configs, dryrun=False, verbose=False):
         deskew = False
         if settings['waveform']['z-motion'] == 'Sample piezo':
             if 's-piezo' not in settings['waveform']:
-                exit(f'error: settings file did not contain a S Piezo field')
+                exit('error: settings file did not contain a S Piezo field')
             if 'interval' not in settings['waveform']['s-piezo']:
-                exit(f'error: settings file did not contain a S Piezo Interval field')
+                exit('error: settings file did not contain a S Piezo Interval field')
 
             deskew = True
 
@@ -276,7 +276,7 @@ def process(dirs, configs, dryrun=False, verbose=False):
         decon = False
         if params_decon:
             if 'laser' not in settings['waveform']:
-                exit(f'error: settings file did not contain a Laser field')
+                exit('error: settings file did not contain a Laser field')
 
             decon = True
 
@@ -303,7 +303,7 @@ def process(dirs, configs, dryrun=False, verbose=False):
                 if deskew:
                     inpath = d / f
                     outpath = output_deskew / tag_filename(f, '_deskew')
-                    tmp = cmd_deskew + f' -w -s %s -o %s %s;' % (steps[ch], outpath , inpath)
+                    tmp = cmd_deskew + ' -w -s %s -o %s %s;' % (steps[ch], outpath , inpath)
                     cmd.append(tmp)
                 if decon:
                     if deskew:
@@ -315,7 +315,7 @@ def process(dirs, configs, dryrun=False, verbose=False):
                     step = settings['waveform']['s-piezo']['interval'][ch] 
                     step = step * math.sin(31.8 * math.pi/180.0)
 
-                    tmp = cmd_decon + f' -w -k %s -p %s -q %s -o %s %s;' % (psfpaths[ch], psfsteps[ch], step, outpath, inpath)
+                    tmp = cmd_decon + ' -w -k %s -p %s -q %s -o %s %s;' % (psfpaths[ch], psfsteps[ch], step, outpath, inpath)
                     cmd.append(tmp)
                 if len(cmd) > 1:
                     cmd = ''.join(cmd)
