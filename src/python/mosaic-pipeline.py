5d915f46f186aa499f709a18d1fa0dd455df48d5
opp1231 <ow.f.puls@gmail.com>
2023-08-29 18:28:33 -0400

    Testing

--- src/python/mosaic-pipeline.py
+++ src/python/mosaic-pipeline.py
@@ -669,8 +669,12 @@ if __name__ == '__main__':
 
     # load config file
     configs = load_configs(args.input)
-    print(configs)
-    sys.stdout.flush()
+    original_stdout = sys.stdout
+    with open('output.txt','w') as file:
+        sys.stdout = file
+        print(configs)
+        sys.stdout.flush()
+    sys.stdout = original_stdout
 
     # get dictionary of processed files
     root_dir = Path(configs['paths']['root'])
